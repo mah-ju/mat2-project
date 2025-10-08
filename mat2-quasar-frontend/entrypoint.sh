446bc86f1b4492a03a29168b0ca1b375b115d4d7
Maria J <mahjuaraujo@gmail.com>
2025-10-08 13:39:54 -0300

    Fix placeholder replacement in index.html

--- mat2-quasar-frontend/entrypoint.sh
+++ mat2-quasar-frontend/entrypoint.sh
@@ -13,9 +13,8 @@ do
 done
 
 echo "Processing index.html ...";
-cp /var/www/html/index.html /var/www/html/index.html.tmpl.html
-envsubst < /var/www/html/index.html.tmpl.html > /var/www/html/index.html
-rm /var/www/html/index.html.tmpl.html
+# Replace placeholder in index.html
+sed -i "s|MAT2_API_URL_PROD_PLACEHOLDER|$MAT2_API_URL_PROD|g" /var/www/html/index.html
 
 cat /var/www/html/index.html
 echo "Starting Nginx"
